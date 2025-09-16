38806a83123530f846c3ea0a48aea5997e480375
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-09-16 14:53:43 -0500

    fix: fixed inactive worker

--- background.js
+++ background.js
@@ -1,15 +1,36 @@
-let words = [];
+// background.js (service worker)
 
-fetch(chrome.runtime.getURL("words.json"))
-  .then(res => res.json())
-  .then(data => words = data);
-
-chrome.action.onClicked.addListener(() => {
-  showRandomWord();
+// Crear alarma al instalar
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create("showWordAlarm", { periodInMinutes: 1 });
 });
 
-function showRandomWord() {
-  if (words.length === 0) return;
+// Escuchar alarma
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "showWordAlarm") {
+    loadWordsAndShow();
+  }
+});
+
+// Escuchar clic en el icono de la extensión
+chrome.action.onClicked.addListener(() => {
+  loadWordsAndShow();
+});
+
+// Cargar JSON y mostrar palabra
+function loadWordsAndShow() {
+  fetch(chrome.runtime.getURL("words.json"))
+    .then(res => res.json())
+    .then(data => {
+      showRandomWord(data);
+    })
+    .catch(err => console.error("Error cargando words.json:", err));
+}
+
+// Mostrar palabra aleatoria
+function showRandomWord(words) {
+  if (!words || words.length === 0) return;
+
   const word = words[Math.floor(Math.random() * words.length)];
 
   chrome.notifications.create({
@@ -20,8 +41,4 @@ function showRandomWord() {
     requireInteraction: true
   });
 }
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.action === "showWord") showRandomWord();
-});
-setInterval(showRandomWord, 60000);
 
